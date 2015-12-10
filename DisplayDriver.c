464a32a4ca929d9c0492dfcfd6d942fb97a5babb
Elisey <elisey.rav@gmail.com>
2015-12-10 22:45:24 +0700

    Обновлен модуль драйвера дисплея

--- DisplayDriver.c
+++ DisplayDriver.c
@@ -34,7 +34,7 @@ void DisplayDriver_Process()
 				Lcd_Goto(2,0);
 			}
 			
-			Lcd_PutChar(videoBuffer[i]);
+			Lcd_PutChar(videoBuffer[i]);	
 		}
 	}
 }
@@ -44,4 +44,50 @@ void DisplayDriver_PutChar(uint8_t line, uint8_t coloumn, uint8_t ch)
 	uint8_t videoDriverIndex = (line * NUM_OF_COLOMNS) + coloumn;
 	
 	videoBuffer[videoDriverIndex] = ch;
+}
+
+void DisplayDriver_PutString(uint8_t line, uint8_t colomn, char *str)
+{
+	while (*str != '\0')	{
+		DisplayDriver_PutChar(line, colomn, *str);
+		str++;	
+		colomn++;
+		if (colomn >= NUM_OF_COLOMNS)	{
+			colomn = 0;
+			line++;
+			if (line >= NUM_OF_LINES)	{
+				line = 0;
+			}
+		}
+	}
+}
+
+void DisplayDriver_Clear()
+{
+	int i;
+	for(i=0; i < VIDEO_BUFFER_SIZE; i++)	{
+		videoBuffer[i] = 0x20;
+		videoBufferCopy[i] = 0;
+	}
+}
+
+char* itoa(int i, char b[])
+{
+    char const digit[] = "0123456789";
+    char* p = b;
+    if(i<0){
+        *p++ = '-';
+        i *= -1;
+    }
+    int shifter = i;
+    do{ //Move to where representation ends
+        ++p;
+        shifter = shifter/10;
+    }while(shifter);
+    *p = '\0';
+    do{ //Move back, inserting digits as u go
+        *--p = digit[i%10];
+        i = i/10;
+    }while(i);
+    return b;
 }
--- DisplayDriver.h
+++ DisplayDriver.h
@@ -5,3 +5,6 @@ void DisplayDriver_Init();
 void DisplayDriver_Process();
 
 void DisplayDriver_PutChar(uint8_t line, uint8_t coloumn, uint8_t ch);
+void DisplayDriver_PutString(uint8_t line, uint8_t coloumn, char *str);
+void DisplayDriver_Clear();
+char* itoa(int i, char b[]);
